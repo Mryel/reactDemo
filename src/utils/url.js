69880d1b12fc8205165ef0b45b0e6358077242ab
叶清来 <xieqinglai@xieqinglaideMacBook-Pro.local>
2019-05-17 23:24:17 +0800

    API信息封装

--- src/utils/url.js
+++ src/utils/url.js
@@ -35,4 +35,14 @@ export default {
     // 获取帖子列表
     getPostList: () => `/post?filter=${encodeFilter(postListFilter)}`,
     // 获取帖子详情
+    getPostById: id => `/post?filter=${encodeFilter(postByIdFilter(id))}`,
+    // 新建帖子
+    createPost: () => '/post',
+    // 修改帖子
+    updatePost: id =>  `/post/$(id)`,
+    // 获取评论列表
+    getCommentList: postId =>
+        `/comment?filter=${encodeFilter(commentListFilter(postId))}`,
+    // 新建评论
+    createComment: () => "/comment"
 }
